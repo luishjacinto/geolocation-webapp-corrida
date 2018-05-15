49b8f322fdb86085c68497eacacf441b36ae5029
LuisHJacinto <henriquejs98@hotmail.com>
2018-05-15 16:33:19 -0300

    Add files via upload

--- mapa.js
+++ mapa.js
@@ -7,6 +7,14 @@
         var map = new google.maps.Map(document.getElementById('map'), {
           center: { lat: -34.397, lng: 150.644 },
           zoom: 17,
+          mapTypeControl: false,
+          streetViewControl: false,
+          rotateControl: false,
+          fullscreenControl: false,
+          zoomControlOptions: {
+            position: google.maps.ControlPosition.RIGHT_CENTER
+          },
+
           styles: [{
               "elementType": "geometry",
               "stylers": [{
