2ae5aa632227416155ce832ac07391977c36c3f8
LuisHJacinto <henriquejs98@hotmail.com>
2018-05-18 19:49:25 -0300

    Add files via upload

--- mapa.js
+++ mapa.js
@@ -2,7 +2,6 @@
       // prompted by your browser. If you see the error "The Geolocation service
       // failed.", it means you probably did not give permission for the browser to
       // locate you.
-      /*
       if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
           mMap.setMyLocationEnabled(true);
@@ -22,198 +21,84 @@
             position: google.maps.ControlPosition.RIGHT_CENTER
           },
 
-          styles: [{
-              "elementType": "geometry",
-              "stylers": [{
-                "color": "#1d2c4d"
-              }]
+          styles: [
+            {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+            {
+              featureType: 'administrative.locality',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
             },
             {
-              "elementType": "labels.text.fill",
-              "stylers": [{
-                "color": "#8ec3b9"
-              }]
+              featureType: 'poi',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
             },
             {
-              "elementType": "labels.text.stroke",
-              "stylers": [{
-                "color": "#1a3646"
-              }]
+              featureType: 'poi.park',
+              elementType: 'geometry',
+              stylers: [{color: '#263c3f'}]
             },
             {
-              "featureType": "administrative.country",
-              "elementType": "geometry.stroke",
-              "stylers": [{
-                "color": "#4b6878"
-              }]
+              featureType: 'poi.park',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#6b9a76'}]
             },
             {
-              "featureType": "administrative.land_parcel",
-              "elementType": "labels.text.fill",
-              "stylers": [{
-                "color": "#64779e"
-              }]
+              featureType: 'road',
+              elementType: 'geometry',
+              stylers: [{color: '#38414e'}]
             },
             {
-              "featureType": "administrative.province",
-              "elementType": "geometry.stroke",
-              "stylers": [{
-                "color": "#4b6878"
-              }]
+              featureType: 'road',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#212a37'}]
             },
             {
-              "featureType": "landscape.man_made",
-              "elementType": "geometry.stroke",
-              "stylers": [{
-                "color": "#334e87"
-              }]
+              featureType: 'road',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#9ca5b3'}]
             },
             {
-              "featureType": "landscape.natural",
-              "elementType": "geometry",
-              "stylers": [{
-                "color": "#023e58"
-              }]
+              featureType: 'road.highway',
+              elementType: 'geometry',
+              stylers: [{color: '#746855'}]
             },
             {
-              "featureType": "landscape.natural.landcover",
-              "elementType": "geometry.fill",
-              "stylers": [{
-                "color": "#a19e7b"
-              }]
+              featureType: 'road.highway',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#1f2835'}]
             },
             {
-              "featureType": "poi",
-              "elementType": "geometry",
-              "stylers": [{
-                "color": "#283d6a"
-              }]
+              featureType: 'road.highway',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#f3d19c'}]
             },
             {
-              "featureType": "poi",
-              "elementType": "labels.text.fill",
-              "stylers": [{
-                "color": "#6f9ba5"
-              }]
+              featureType: 'transit',
+              elementType: 'geometry',
+              stylers: [{color: '#2f3948'}]
             },
             {
-              "featureType": "poi",
-              "elementType": "labels.text.stroke",
-              "stylers": [{
-                "color": "#1d2c4d"
-              }]
+              featureType: 'transit.station',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
             },
             {
-              "featureType": "poi.park",
-              "elementType": "geometry.fill",
-              "stylers": [{
-                "color": "#023e58"
-              }]
+              featureType: 'water',
+              elementType: 'geometry',
+              stylers: [{color: '#17263c'}]
             },
             {
-              "featureType": "poi.park",
-              "elementType": "labels.text.fill",
-              "stylers": [{
-                "color": "#3C7680"
-              }]
+              featureType: 'water',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#515c6d'}]
             },
             {
-              "featureType": "road",
-              "elementType": "geometry",
-              "stylers": [{
-                "color": "#304a7d"
-              }]
-            },
-            {
-              "featureType": "road",
-              "elementType": "labels.text.fill",
-              "stylers": [{
-                "color": "#98a5be"
-              }]
-            },
-            {
-              "featureType": "road",
-              "elementType": "labels.text.stroke",
-              "stylers": [{
-                "color": "#1d2c4d"
-              }]
-            },
-            {
-              "featureType": "road.highway",
-              "elementType": "geometry",
-              "stylers": [{
-                "color": "#2c6675"
-              }]
-            },
-            {
-              "featureType": "road.highway",
-              "elementType": "geometry.fill",
-              "stylers": [{
-                "saturation": 40
-              }]
-            },
-            {
-              "featureType": "road.highway",
-              "elementType": "geometry.stroke",
-              "stylers": [{
-                "color": "#255763"
-              }]
-            },
-            {
-              "featureType": "road.highway",
-              "elementType": "labels.text.fill",
-              "stylers": [{
-                "color": "#b0d5ce"
-              }]
-            },
-            {
-              "featureType": "road.highway",
-              "elementType": "labels.text.stroke",
-              "stylers": [{
-                "color": "#023e58"
-              }]
-            },
-            {
-              "featureType": "transit",
-              "elementType": "labels.text.fill",
-              "stylers": [{
-                "color": "#98a5be"
-              }]
-            },
-            {
-              "featureType": "transit",
-              "elementType": "labels.text.stroke",
-              "stylers": [{
-                "color": "#1d2c4d"
-              }]
-            },
-            {
-              "featureType": "transit.line",
-              "elementType": "geometry.fill",
-              "stylers": [{
-                "color": "#283d6a"
-              }]
-            },
-            {
-              "featureType": "transit.station",
-              "elementType": "geometry",
-              "stylers": [{
-                "color": "#3a4762"
-              }]
-            },
-            {
-              "featureType": "water",
-              "elementType": "geometry",
-              "stylers": [{
-                "color": "#0e1626"
-              }]
-            },
-            {
-              "featureType": "water",
-              "elementType": "labels.text.fill",
-              "stylers": [{
-                "color": "#4e6d70"
-              }]
+              featureType: 'water',
+              elementType: 'labels.text.stroke',
+              stylers: [{color: '#17263c'}]
             }
           ]
         });
@@ -222,37 +107,46 @@
         // Try HTML5 geolocation.
         if (navigator.geolocation) {
           setInterval(
-          navigator.geolocation.getCurrentPosition(function (position) {
+          navigator.geolocation.watchPosition(function (position) {
+            console.log(position);
+
             var pos = {
               lat: position.coords.latitude,
               lng: position.coords.longitude,
             };
-            console.log("funfa");
+
             infoWindow.setPosition(pos);
             infoWindow.setContent('Local Encontrado.');
             map.setCenter(pos);
-          }, function () {
+          }, function (Error) {
             handleLocationError(true, infoWindow, map.getCenter());
-        })
+          }, { enableHighAccuracy: true, maximumAge: 5000, timeout: 5000})
           , 5000);
         } else {
           // Browser doesn't support Geolocation
           handleLocationError(false, infoWindow, map.getCenter());
         }
       }
-
+/*
+    if('geolocation' in navigator){
+      const watcher = navigator.geolocation.watchPosition(function(position){
+        console.log(position);
+        console.log(position.coords.latitude + " " + position.coords.longitude);
+        /*var pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+        console.log(pos);
+        map.setCenter(pos);
+      }, function(error){
+        console.log(error);
+      }, { enableHighAccuracy: true, maximumAge: 5000, timeout: 5000})
+    }
+  */
       function handleLocationError(browserHasGeolocation, infoWindow, pos) {
         infoWindow.setPosition(pos);
         infoWindow.setContent(browserHasGeolocation ?
           'Error: The Geolocation service failed.' :
           'Error: Your browser doesn\'t support geolocation.');
       }
-      */
-      if('geolocation' in navigator){
-        const watcher = navigator.geolocation.watchPosition(function(position){
-          console.log(position);
-          document.getElementById("header").innerHTML = position.coords.latitude + " " + position.coords.longitude;
-        }, function(error){
-          console.log(error);
-        }, { enableHighAccuracy: true, maximumAge: 5000, timeout: 5000})
-      }
+  
